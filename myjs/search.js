346ed9776ba7d5fcb7af8ae20fe9645fbfaaa8f1
Alif Wahid <alif@wahid>
2019-03-31 19:46:38 +1300

    Display full excerpt in search results.
    Signed-off-by: Alif Wahid <alif@wahid>

--- myjs/search.js
+++ myjs/search.js
@@ -12,7 +12,7 @@
             for (var i = 0; i < results.length; i++) {
                 var item = store[results[i].ref];
                 appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3>';
-                appendString += '<p>' + item.excerpt.substring(0, 150) + '...</p></a></li>';
+                appendString += '<p>' + item.excerpt + '</p></a></li>';
             }
             searchResults.innerHTML = appendString;
         } else {
